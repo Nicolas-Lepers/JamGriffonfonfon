3d8a51c8339842a93e148a2d69c7f3a7065e33de
P3D\antoine.foucault <antoine.>
2024-11-19 16:15:02 +0100

    [ART] All Sprites on Scriptables 🧢

--- Assets/Scripts/CardInfo.cs
+++ Assets/Scripts/CardInfo.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardInfo : MonoBehaviour
 {
     public CardData CardDataRef;
+    public Image Image;
 }
