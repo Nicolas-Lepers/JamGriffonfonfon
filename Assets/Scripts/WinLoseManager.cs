7d75f910ebc06dcc81f4176b08f35c0aaaa422d5
P3D\antoine.foucault <antoine.>
2024-11-19 17:16:35 +0100

    [PROGRAMMING] WinLose Text 🎨

--- Assets/Scripts/WinLoseManager.cs
+++ Assets/Scripts/WinLoseManager.cs
@@ -1,8 +1,22 @@
+using System.Collections;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class WinLoseManager : MonoBehaviour
 {
+    [SerializeField] private Transition _transition;
+    [SerializeField] private float _waitTransitionTime;
+    [SerializeField] private TMP_Text _winText;
+    [SerializeField] private TMP_Text _loseText;
+    
+    [Header("Polish")]
+    [SerializeField] private float _textMoveOffset;
+    [SerializeField] private float _textMoveTime;
+    [SerializeField] private Ease _textMoveEase;
+    
     private void Start()
     {
         GameManager.Instance.OnWin += Win;
@@ -15,13 +29,32 @@ public class WinLoseManager : MonoBehaviour
         GameManager.Instance.OnWin -= Lose;
     }
 
+    [ContextMenu("Win")]
     private void Win()
     {
-        Debug.Log("Win");
+        _winText.gameObject.SetActive(true);
+        MoveText(_winText);
+        StartCoroutine(WaitForTransition());
     }
-
+    
+    [ContextMenu("Lose")]
     private void Lose()
     {
-        Debug.Log("Lose");
+        _loseText.gameObject.SetActive(true);
+        MoveText(_loseText);
+        StartCoroutine(WaitForTransition());
+    }
+    private void MoveText(TMP_Text text)
+    {
+        var currentY = text.transform.hierarchyCapacity;
+
+        text.transform.position = new Vector3(_winText.transform.position.x, currentY + _textMoveOffset, _winText.transform.position.z);
+        text.transform.DOMoveY(currentY, _textMoveTime).SetEase(_textMoveEase);
+    }
+
+    private IEnumerator WaitForTransition()
+    {
+        yield return new WaitForSeconds(_waitTransitionTime);
+        _transition.SetTransition(() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex));
     }
 }
